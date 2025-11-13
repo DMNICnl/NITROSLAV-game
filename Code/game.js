42676cb1d1f260bb73098b2fa17bd9c98b5620e6
dmnic <dmnicnl@gmail.com>
2025-11-13 14:52:08 +0100

    loading animating map paralax demo into HTML canvas with JS

--- Code/game.js
+++ Code/game.js
@@ -30,18 +30,82 @@ function initGame() {
   const carImg = new Image();
   carImg.src = carFrames[0];
 
-  function drawCar() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.drawImage(carImg, 300, 600);
+
+  const BAlayerFiles = [
+    "../bratislava/demobratislavamap/BAsky.png", //0
+    "../bratislava/demobratislavamap/BAnatureNcastle.png", // 1
+    "../bratislava/demobratislavamap/BAhouses.png", //2
+    "../bratislava/demobratislavamap/BAlampposts.png", //3
+    "../bratislava/demobratislavamap/BAroad.png", //4
+    "../bratislava/demobratislavamap/BAclouds.png", //5
+    "../bratislava/demobratislavamap/BADayFilter.png", //6
+  ];
+  const layers = [];
+  let loaded = 0;
+
+  
+  
+  for (let file of BAlayerFiles) {
+    const img = new Image();
+    img.src = file;
+    img.onload = () => {
+      loaded++;
+      if (loaded === BAlayerFiles.length) {
+        //* startGame();
+      }
+    };
+    layers.push(img);
   }
 
+  function drawAll() {
+    layers.forEach((img) => ctx.drawImage(img, 0, 0));
+  }
+  
+  function drawCar() {
+    ctx.drawImage(carImg, 300, 600);
+  }
+let roadX = 0;
+let housesX = 0;
+let cloudsX = 0;
+let natureX = 0;
+let lammpostX = 0;
+
+let scrollSpeed = 200;
+let lastTime = performance.now();
+
+function loopLayer(layerImg, xPos, speed, deltaTime){
+  xPos -= speed * (deltaTime / 1000);
+  if(xPos <= -canvas.width){
+    xPos += canvas.width;
+  }
+  ctx.drawImage(layerImg, xPos, 0, canvas.width, canvas.height);
+  ctx.drawImage(layerImg, xPos + canvas.width, 0, canvas.width, canvas.height);
+
+  return xPos;
+}
+  function drawFrame(timestamp){
+    const deltaTime = timestamp - lastTime;
+    lastTime = timestamp;
+    ctx.clearRect(0,0, canvas.width, canvas.height);
+
+
+    ctx.drawImage(layers[0], 0,0, canvas.width, canvas.height)
+    natureX = loopLayer(layers[1], natureX, scrollSpeed * 0.5, deltaTime);
+    housesX = loopLayer(layers[2], housesX, scrollSpeed * 0.75, deltaTime);
+    lammpostX = loopLayer(layers[3],lammpostX, scrollSpeed *1.3 , deltaTime)
+    roadX = loopLayer(layers[4], roadX, scrollSpeed * 1.5, deltaTime);
+    cloudsX = loopLayer(layers[5], cloudsX, scrollSpeed * 0.25, deltaTime);
+    ctx.drawImage(layers[6], 0,0, canvas.width, canvas.height)
+    drawCar();
+    requestAnimationFrame(drawFrame);
+  }
   setInterval(() => {
     frameIndex = (frameIndex + 1) % carFrames.length;
     carImg.src = carFrames[frameIndex];
-    drawCar();
   }, 300);
+  
+  carImg.onload = () =>   requestAnimationFrame(drawFrame);
 
-  carImg.onload = drawCar;
 }
 
 // animation frames for all cars
@@ -207,4 +271,3 @@ function handleContinueToGame() {
     "this is also the map path" + sessionStorage.getItem("choosenMapPath")
   );
 }
-
